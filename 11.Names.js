a982d624f045be8a03bc96357444983998172394
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:37:33 +0500

    Add files via upload

---
+++ 11.Names.js
@@ -0,0 +1,11 @@
+"use strict";
+//Define an array of friends names
+let names = ["Saba", "Anum", "kashaf"];
+//print each name
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+//OR
+names.forEach(name => {
+    console.log(name);
+});
---
+++ 11.Names.ts
@@ -0,0 +1,10 @@
+//Define an array of friends names
+let names :string[] = ["Saba","Anum","kashaf"];
+//print each name
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+//OR
+names.forEach(name => {
+    console.log(name);
+});
