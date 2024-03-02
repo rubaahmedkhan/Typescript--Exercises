01ff6826ba9b004b7f455a04b1873d073025889d
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:41:43 +0500

    Add files via upload

---
+++ 12. Greeting.js
@@ -0,0 +1,9 @@
+"use strict";
+//store names in array
+let names = ["Saba", "Anum", "Kashaf"];
+//message
+let message = "Do you like to learn mathametics";
+//Greeting personalized with each person's name
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
---
+++ 12.Greeting.ts
@@ -0,0 +1,9 @@
+//store names in array
+let names : string[ ] = ["Saba","Anum","Kashaf"];
+//message
+let message : string = "Do you like to learn mathametics";
+
+//Greeting personalized with each person's name
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
