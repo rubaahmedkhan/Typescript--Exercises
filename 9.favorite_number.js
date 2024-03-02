cb91fe3f17d781f02e93a3572910c38097fa5e96
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:21:18 +0500

    Add files via upload

---
+++ 9.favorite_number.js
@@ -0,0 +1,6 @@
+"use strict";
+//store favorite number
+const favorite_number = 4;
+//create message
+const message = " My lucky number is " + favorite_number;
+console.log(message);
---
+++ 9.favorite_number.ts
@@ -0,0 +1,5 @@
+//store favorite number
+const favorite_number :number = 4;
+//create message
+const message :string = " My lucky number is " + favorite_number  ;
+console.log(message);
