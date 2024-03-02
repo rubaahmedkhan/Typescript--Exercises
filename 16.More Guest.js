4aa7c9484750ba11ea617c41c4b56fc6047a0eda
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 22:23:36 +0500

    Add files via upload

---
+++ 16.More Guest.js
@@ -0,0 +1,27 @@
+"use strict";
+let guest = ['Fatima', 'Ali', 'Hussain'];
+for (var i = 0; i < guest.length; i++) {
+    //console.log("Dear" + " " + guest[i] + " " + " you are invited to the dinner");
+}
+;
+//changing guest list 
+let cannot_attend = guest.splice(1, 1)[0];
+let new_guest = "kashaf";
+//console.log("".concat(cannot_attend, "can't make it to the dinner"));
+//add new guest 
+guest.push(new_guest);
+//print invitation message 
+for (var i = 0; i < guest.length; i++) {
+    // console.log("Dear" + " " + guest[i] + " " + " you are invited to the dinner");
+}
+;
+console.log("We found a bigger dinner table.");
+//Adding three new guest
+guest.unshift("Hira");
+guest.splice(Math.floor(guest.length / 2), 0, "Aqsa");
+guest.push("Taha");
+//print invitation message
+for (let i = 0; i < guest.length; i++) {
+    console.log("Dear" + " " + guest[i] + " " + " you are invited to the dinner");
+}
+;
