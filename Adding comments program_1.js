c1a3b472a55438764f5153ec69408ced3e8b735a
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:32:03 +0500

    Delete Adding comments program_1.js

--- Adding comments program_1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// program written by :Ruba Ahmed Khan
-//current date :16-2-2024
-//This program will concatinate two strings
-let a = 'hello';
-let b = 'everyone';
-let c = a + b;
-console.log(c);
