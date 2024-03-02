877770c16b1f5cee9d02fad4ceb076bd5ec0f202
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:43:49 +0500

    Add files via upload

---
+++ quote.ts
@@ -0,0 +1,3 @@
+const famous_person :string = 'Albert Eintein';
+const message :string = '"A person who never a mistake never tried anything new"';
+console.log(`${famous_person} once said, ${message}`);
