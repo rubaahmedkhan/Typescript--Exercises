df38683065324f5302b3cd09a4fcc07c2146f22b
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:56:40 +0500

    Delete quote.js

--- quote.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const famous_person = 'Albert Eintein';
-const message = '"A person who never a mistake never tried anything new"';
-console.log(`${famous_person} once said, ${message}`);
