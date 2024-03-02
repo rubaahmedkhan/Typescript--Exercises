28c9fed5044a41338665f0d1d5bd0a6442cb37f0
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:33:44 +0500

    Add files via upload

---
+++ name.ts
@@ -0,0 +1,11 @@
+// store person name in a variable
+let personName:string = "Ruba khan";
+
+//print person name in lower case
+console.log(personName.toLowerCase());
+
+//print person name in uppercase
+console.log(personName.toUpperCase());
+
+//print person name in titlecase
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
