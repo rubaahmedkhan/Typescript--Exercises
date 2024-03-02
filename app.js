2e1321b4a1d10a07566465de4a048693a21bf7b1
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:31:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+// store person's in a variable
+let personName = "Eric";
+//print a message to that person
+console.log(`hello ${personName} would you like to learn some python today`);
+//OR
+console.log("hello" + " " + personName + " " + "would you like to learn python");
