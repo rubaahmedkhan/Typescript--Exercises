6fe75063adc84576de5c473b20f8f0e625dc9087
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:45:41 +0500

    Add files via upload

---
+++ 14.Guest List.js
@@ -0,0 +1,6 @@
+"use strict";
+let guest = ['Fatima', 'Ali', 'Hussain'];
+for (let i = 0; i < guest.length; i++) {
+    console.log("Dear" + " " + guest[i] + " " + " you are invited to the dinner");
+}
+;
---
+++ 14.Guest List.ts
@@ -0,0 +1,4 @@
+let guest :string []=['Fatima','Ali','Hussain'];
+for(let i=0; i<guest.length; i++){
+    console.log("Dear" + " " + guest[i] + " " + " you are invited to the dinner" )
+}  ;   
