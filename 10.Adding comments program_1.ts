e0f7489bb1d5a945ff406a2c1ebcd62c069158a4
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:36:07 +0500

    Add files via upload

---
+++ 10.Adding comments program_1.ts
@@ -0,0 +1,8 @@
+// program written by :Ruba Ahmed Khan
+//current date :16-2-2024
+
+//This program will concatinate two strings
+ let a :string ='hello';
+ let b :string = 'everyone';
+ let c =a+b;
+ console.log(c);
