a34518d4854a72f04ed0e430b2c8c9e420fd9983
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:43:53 +0500

    Add files via upload

---
+++ 13.your own array.js
@@ -0,0 +1,6 @@
+"use strict";
+//list of favorite modes of transportation
+let transportation = ["Honda City", "Unique", "Ninja H2R"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a" + " " + transportation[i]);
+}
---
+++ 13.your own array.ts
@@ -0,0 +1,6 @@
+//list of favorite modes of transportation
+let transportation : string []=["Honda City","Unique","Ninja H2R"];
+
+for(let i=0; i<transportation.length; i++){
+    console.log("I would like to own a" + " " + transportation[i])
+}
