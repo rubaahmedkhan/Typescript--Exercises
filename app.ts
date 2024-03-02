77a2333c17d41f1b58310f0de70f23859a8dae05
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:54:19 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-// store person's in a variable
-let personName:string = "Eric";
-
-//print a message to that person
-console.log(`hello ${personName} would you like to learn some python today`);
-//OR
-console.log("hello" +" " +personName + " " + "would you like to learn python")
