54fb89c3e9d9a82380b35461d915291860147c56
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:14:10 +0500

    Add files via upload

---
+++ 5.famous Quote 2.js
@@ -0,0 +1,4 @@
+"use strict";
+const famous_person = 'Albert Eintein';
+const message = '"A person who never a mistake never tried anything new"';
+console.log(`${famous_person} once said, ${message}`);
