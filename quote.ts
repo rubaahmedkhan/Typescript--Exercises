43a2225c66c6db94813a2798b481ea1d561990a1
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:56:56 +0500

    Delete quote.ts

--- quote.ts
+++
@@ -1,3 +0,0 @@
-const famous_person :string = 'Albert Eintein';
-const message :string = '"A person who never a mistake never tried anything new"';
-console.log(`${famous_person} once said, ${message}`);
