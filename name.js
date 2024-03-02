7d73a6ba41fc7b215aed42acbd1b0943987c70d4
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 20:55:15 +0500

    Delete name.js

--- name.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// store person name in a variable
-let personName = "Ruba khan";
-//print person name in lower case
-console.log(personName.toLowerCase());
-//print person name in uppercase
-console.log(personName.toUpperCase());
-//print person name in titlecase
-console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
