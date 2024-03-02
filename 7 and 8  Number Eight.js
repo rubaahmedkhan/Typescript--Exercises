400694557329d29076c19b126089f8ed5b33dff0
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-02 21:19:11 +0500

    Add files via upload

---
+++ 7 and 8  Number Eight.js
@@ -0,0 +1,9 @@
+"use strict";
+// Addition
+console.log(5 + 3);
+//subtraction
+console.log(9 - 1);
+//multiplication
+console.log(2 * 4);
+//Division
+console.log(16 / 2);
---
+++ 7 and 8 Number Eight.ts
@@ -0,0 +1,8 @@
+// Addition
+ console.log(5+3);
+ //subtraction
+ console.log(9-1);
+ //multiplication
+ console.log(2*4);
+ //Division
+ console.log(16/2);
