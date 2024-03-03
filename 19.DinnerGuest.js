e2037c6da31d8f3e310b159bcd79a79394cdbd31
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:12:07 +0500

    Add files via upload

---
+++ 19.DinnerGuest.js
@@ -0,0 +1,4 @@
+"use strict";
+let guest = ['Fatima', 'Ali', 'Hussain'];
+//print a message indicating the number of people you are inviting to dinner
+console.log(`Number of people invited to dinner: ${guest.length}`);
---
+++ 19.DinnerGuest.ts
@@ -0,0 +1,3 @@
+let guest :string []=['Fatima','Ali','Hussain'];
+//print a message indicating the number of people you are inviting to dinner
+console.log(`Number of people invited to dinner: ${guest.length}`);
