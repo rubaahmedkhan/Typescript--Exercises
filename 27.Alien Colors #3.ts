acbda4a147ceb414f7feedacb791299bcf01d734
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:38:22 +0500

    Add files via upload

---
+++ 27.Alien Colors #3.ts
@@ -0,0 +1,8 @@
+let alien_color:string = "green";
+if(alien_color === "green"){
+    console.log("You earned 5 points");
+}else if(alien_color ==="yellow"){
+    console.log("You earned 10 points");
+}else if(alien_color==="red"){
+    console.log("You earned 15 points")
+};
