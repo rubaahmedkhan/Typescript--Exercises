a707ce8b6737b92e1e02888493ce29e93acb93c3
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:42:20 +0500

    Add files via upload

---
+++ 29.Favorite Fruit.ts
@@ -0,0 +1,22 @@
+let Favorite_Fruits: string[] =["orange","grapes","apple"];
+
+//check if orange is in the array
+if (Favorite_Fruits.includes("orange")){
+    console.log("I like oranges.");
+}
+    //check if grapes is in the array
+  if (Favorite_Fruits.includes("grapes")){
+    console.log("You really like grapes.");
+  }
+  // check if apple is in the array
+  if (Favorite_Fruits.includes("apple")){
+    console.log("You really like apples.");
+  }
+  // check if mango is in the array
+  if (Favorite_Fruits.includes("mango")){
+    console.log("You really like mango!");
+  }
+  //check if cherry is in the array
+  if (Favorite_Fruits.includes("cherry")){
+    console.log("You really like cherry.");
+  }
