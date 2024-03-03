a5096b708f33795ed469b3396747a6c4e127d753
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:20:01 +0500

    Add files via upload

---
+++ 22.Intentional Error.js
@@ -0,0 +1,4 @@
+"use strict";
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(array[10]); // error
+console.log(array[4]);
---
+++ 22.Intentional Error.ts
@@ -0,0 +1,3 @@
+let array =[1,2,3,4,5,6,7,8]
+console.log(array[10]); // error
+console.log(array[4]);
