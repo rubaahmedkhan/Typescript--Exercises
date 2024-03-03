0ff957ea1479aadfa896cc91c35d6e55f3096dba
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:17:03 +0500

    Add files via upload

---
+++ 21.object.js
@@ -0,0 +1,16 @@
+"use strict";
+//Define object
+let person;
+//initialize object
+person = {
+    name: "Ruba khan",
+    age: 23,
+    gender: "Female",
+};
+console.log(person);
+person = {
+    name: "Ali",
+    age: 34,
+    gender: "Male"
+};
+console.log(person);
---
+++ 21.object.ts
@@ -0,0 +1,19 @@
+//Define object
+let person : {
+    name : string,
+    age : number,
+    gender : string,
+};
+//initialize object
+person = {
+    name : "Ruba khan",
+    age :  23,
+    gender : "Female",
+};
+console.log(person);
+person = {
+    name : "Ali",
+    age : 34,
+    gender : "Male"
+};
+console.log(person);
