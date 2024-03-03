66b84e045b8e13f39861278c533e6d526b945a7e
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:25:33 +0500

    Add files via upload

---
+++ 23.Conditional Test.ts
@@ -0,0 +1,47 @@
+let car : string = 'subaro';
+
+
+//test 1
+console.log("Is car =='subaru'? Ipredict true.");
+console.log(car=='subaro');
+
+//test 2 
+console.log("Is car !='toyota'? I predict true.");
+console.log(car != 'toyota');
+
+//test 3 case sensitive
+console.log("Is cat ==='Subaro'? I predict true");
+console.log(car === 'subaro');
+
+//test 4
+console.log("Is car !=='toyota,? I predict true.");
+console.log(car !=='toyota');
+
+//test 5
+let temperature =25;
+console.log("Is temperature <=16 ? I predict false");
+console.log(temperature<=16);
+
+//test 6
+console.log("Is car.length > 0 ? I predict  true");
+console.log(car.length>0);
+
+//test 7
+console.log("Is car.length<5? I predict false.");
+console.log(car.length<5);
+
+//test 8
+console.log("Is the character at index 2 in car'a'? I predict false.");
+console.log(car.charAt(2) ==='a');
+
+//test 9
+let age = 18 ;
+console.log("Is age>=19? I predict false. ");
+console.log(age>=19);
+
+//test10
+console.log("Is car.lenth >9 ? I predect false");
+console.log(car.length> 9);
+
+
+
