a3c0c7e4c1997913dd03d3dfdc5f4f3b860d6b8c
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-03-03 09:15:09 +0500

    Add files via upload

---
+++ 20.Array.ts
@@ -0,0 +1,6 @@
+//Define an array of countries
+const countries: string[] = ['china','pakistan','srilanka','india'];
+
+//print each country in the array
+console.log("list of countries:");
+countries.forEach(country => console.log(country));
